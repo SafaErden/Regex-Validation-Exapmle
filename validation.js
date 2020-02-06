b1766b32c8a52994bb741274908686c43ab43ae0
SafaErden <60184961+SafaErden@users.noreply.github.com>
2020-02-06 20:04:18 +0300

    validation

---
+++ validation.js
@@ -0,0 +1,32 @@
+// validation script here
+const inputs=document.querySelectorAll('input');
+
+const patterns={
+    telephone:/^\d{11}$/,
+    username:/^[a-z\d]{5,12}$/i,
+    password:/^[\w@-]{8,12}$/,
+    slug:/^[a-z\d-]{8,12}$/,
+    email:/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+
+};
+
+function validate(field,regex){
+   if( regex.test(field.value)){
+       field.className='valid';
+
+   }
+   else{
+       field.className='invalid';
+   }
+
+}
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup',(e)=>{
+
+        //console.log(e.target.attributes.name.value);
+        validate(e.target,patterns[e.target.attributes.name.value])
+    });
+
+
+});
